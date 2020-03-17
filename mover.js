d0464dbc1e61dda4a9de459b15cd8cdc56b5daaf
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2020-03-17 19:58:02 -0400

    Initial Commit

---
+++ mover.js
@@ -0,0 +1,51 @@
+class Mover {
+
+  constructor(x, y) {
+    console.log(x, y);
+    this.location = createVector(x, y);
+    this.velocity = createVector(0, 0); //p5.Vector.random2D().mult(3);
+    this.size = 2;
+    this.acceleration = p5.Vector.random2D();
+    this.acceleration.mult(.02);
+    this.topSpeed = 10;
+  }
+
+  update() {
+    let mouse = createVector(mouseX, mouseY);
+    mouse.sub(this.location);
+    mouse.setMag(.1);
+    this.acceleration = mouse;
+
+
+
+    this.velocity = this.velocity.add(this.acceleration);
+
+    this.location = this.location.add(this.velocity);
+
+
+    this.velocity.limit(5);
+
+  }
+
+  checkEdges() {
+    if (this.location.x > width) {
+      this.velocity.x = this.velocity.x * -1;
+    } else if (this.location.x < 0) {
+      this.velocity.x = this.velocity.x * -1;
+    }
+    if (this.location.y > height) {
+      this.velocity.y = this.velocity.y * -1;
+    } else if (this.location.y < 0) {
+      this.velocity.y = this.velocity.y * -1;
+    }
+  }
+
+  display() {
+    //console.log(this.location);
+    noStroke();
+    fill(55);
+    ellipse(this.location.x, this.location.y, this.size, this.size);
+  }
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,47 @@
+//Example from P. 52 of NOC.
+//Mover with acceleration.
+//Tutorial 1.5 Acceleration - The Nature of Code.
+
+var ball;
+var balls = [];
+const qty = 100;
+
+function setup() {
+  createCanvas(640, 480);
+
+  for (let i = 0; i < qty; i++) {
+
+    ball = new Mover(random(width), random(height));
+    balls.push(ball);
+  }
+
+  background(127);
+  noStroke();
+  fill(155);
+
+  // let pos = createVector(12, 13);
+  // console.log(pos);
+  // pos.x = pos.x + 1
+  // pos.y = pos.y + 1
+
+  // console.log(pos);
+  // let one = createVector(1, 1);
+  // console.log(one);
+
+  // pos = pos.add(one);
+
+  // console.log(pos);
+  colorMode(HSB, 360, 100, 100, 1.0);
+  background(270, 50, 50, .5);
+
+}
+
+function draw() {
+  // background(200);
+
+  for (let i = 0; i < qty; i++) {
+    balls[i].update();
+    balls[i].checkEdges();
+    balls[i].display();
+  }
+}
