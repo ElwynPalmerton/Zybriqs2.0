abce11f89832b300ff516254623d047b32f5c46a
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2020-06-16 20:04:20 -0400

    Wahtever

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const ejs = require("ejs");
 const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
-const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 //mongo store.
