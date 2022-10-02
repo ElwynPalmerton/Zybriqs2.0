5c1f626f4e340f47645132b51e66d3688640ce22
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2022-10-02 15:24:42 -0700

    Remove backend routes and save/load functionality in index

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 //mongo store.
-const mongoStore = require("connect-mongo")(session);
+// const mongoStore = require("connect-mongo")(session);
 
 const app = express();
 
@@ -37,47 +37,47 @@ app.use(
   })
 );
 
-let db;
+// let db;
 
-app.use(
-  session({
-    store: new mongoStore({
-      url: process.env.DATABASE_URL,
-    }),
-    // secret: process.env.SECRET,
-    secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
-      secure: false,
-    },
-  })
-);
+// app.use(
+//   session({
+//     store: new mongoStore({
+//       url: process.env.DATABASE_URL,
+//     }),
+//     // secret: process.env.SECRET,
+//     secret: process.env.SECRET,
+//     resave: true,
+//     saveUninitialized: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24,
+//       secure: false,
+//     },
+//   })
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-mongoose
-  .connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((dbConnection) => {
-    db = dbConnection;
-  })
-  .catch((error) => {
-    console.log("Mongo connection error:", error);
-  });
+// mongoose
+//   .connect(process.env.DATABASE_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then((dbConnection) => {
+//     db = dbConnection;
+//   })
+//   .catch((error) => {
+//     console.log("Mongo connection error:", error);
+//   });
 
-mongoose.set("useCreateIndex", true);
+// mongoose.set("useCreateIndex", true);
 
-passport.use(User.createStrategy());
+// passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
-let tempZybriq; //I probably don't need this.
+// let tempZybriq; //I probably don't need this.
 
 /////////////ROOT///////////////
 app.get("/", (req, res) => {
@@ -88,97 +88,97 @@ app.get("/", (req, res) => {
   //res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 //register routes.
-app.use("/register", registerRoutes);
+// app.use("/register", registerRoutes);
 
-app.use("/restore", restoreRoutes);
+// app.use("/restore", restoreRoutes);
 
-app.get("/restore", (req, res) => {
-  // res.sendFile(path.join(__dirname, "client", "index.html"));
-  res.render("pages/index", {
-    user: req.user,
-  });
-  //I don't know how to reference the __dirname to a differenct folder using this command or whatever it is I need to do.
-});
+// app.get("/restore", (req, res) => {
+//   // res.sendFile(path.join(__dirname, "client", "index.html"));
+//   res.render("pages/index", {
+//     user: req.user,
+//   });
+//   //I don't know how to reference the __dirname to a differenct folder using this command or whatever it is I need to do.
+// });
 
-app.use("/login", loginRoutes);
+// app.use("/login", loginRoutes);
 
-app.use("/saveName", saveRoutes);
+// app.use("/saveName", saveRoutes);
 
-app.use("/loadSavedNames", loadSavedRoutes);
+// app.use("/loadSavedNames", loadSavedRoutes);
 
-app.use("/delete", deleteRoutes);
+// app.use("/delete", deleteRoutes);
 
-app.get("/logout", (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.redirect("/");
-});
+// app.get("/logout", (req, res) => {
+//   req.logout();
+//   req.session.destroy();
+//   res.redirect("/");
+// });
 
-///Save over existing Zybriq.
-app.get("/saveOver", (req, res) => {
-  //Gets a list of all saved Zybriqs and sends it to the pages/saveOver.ejs view.
-  let zibNames = [];
-  let zibIds = [];
+// ///Save over existing Zybriq.
+// app.get("/saveOver", (req, res) => {
+//   //Gets a list of all saved Zybriqs and sends it to the pages/saveOver.ejs view.
+//   let zibNames = [];
+//   let zibIds = [];
 
-  for (let zib of req.user.Zybriqs) {
-    zibNames.push(zib.name);
-    zibIds.push(zib._id);
-  }
+//   for (let zib of req.user.Zybriqs) {
+//     zibNames.push(zib.name);
+//     zibIds.push(zib._id);
+//   }
 
-  console.log("Rendering maxiumum Zybs");
+//   console.log("Rendering maxiumum Zybs");
 
-  res.render("pages/saveOver.ejs", {
-    user: req.user,
-    zibNames: zibNames,
-    zibIds: zibIds,
-  });
-});
+//   res.render("pages/saveOver.ejs", {
+//     user: req.user,
+//     zibNames: zibNames,
+//     zibIds: zibIds,
+//   });
+// });
 
-app.get("/success", (req, res) => {
-  res.render("pages/saveSuccess", {
-    user: req.user,
-    message: "Success",
-    id: req.user.tempID,
-  });
-});
+// app.get("/success", (req, res) => {
+//   res.render("pages/saveSuccess", {
+//     user: req.user,
+//     message: "Success",
+//     id: req.user.tempID,
+//   });
+// });
 
-app.get("/registerSuccess", (req, res) => {
-  console.log("in registerSuccess route.");
+// app.get("/registerSuccess", (req, res) => {
+//   console.log("in registerSuccess route.");
 
-  console.log("isAuth ", req.isAuthenticated());
-  if (req.isAuthenticated()) {
-    res.render("pages/registerSuccess", {
-      user: req.user,
-    });
-  } else {
-    res.redirect("/login");
-  }
-});
+//   console.log("isAuth ", req.isAuthenticated());
+//   if (req.isAuthenticated()) {
+//     res.render("pages/registerSuccess", {
+//       user: req.user,
+//     });
+//   } else {
+//     res.redirect("/login");
+//   }
+// });
 
-app.get("/getValue", (req, res) => {
-  console.log(req.session);
-});
+// app.get("/getValue", (req, res) => {
+//   console.log(req.session);
+// });
 
-app.get("/about", (req, res) => {
-  res.render("pages/about", {
-    user: req.user,
-  });
-});
+// app.get("/about", (req, res) => {
+//   res.render("pages/about", {
+//     user: req.user,
+//   });
+// });
 
 //This is called form listSaved.ejs after the radio button for the saved Zibriq is selected.
 //Redirects to /restre?savedZib=  _ID.
-app.post("/loadState", (req, res) => {
-  let savedZybriq = req.body.name; //This is the mongo ID for the current Zybriq.
-  //This is re-rerouted from saveZybriq as req.user.tempID to saveSuccess and then retrieved here and sent to restore.
-  //but I should (???)just use req.session to store it and
-  //avoid all the spaghetti.
-  //
-  //!!!!!!!!!!!!!!!
-  //Save it to req.session.zybID in /saveZybriq (save-routes.js).
-  //and then just use that here.
+// app.post("/loadState", (req, res) => {
+//   let savedZybriq = req.body.name; //This is the mongo ID for the current Zybriq.
+//   //This is re-rerouted from saveZybriq as req.user.tempID to saveSuccess and then retrieved here and sent to restore.
+//   //but I should (???)just use req.session to store it and
+//   //avoid all the spaghetti.
+//   //
+//   //!!!!!!!!!!!!!!!
+//   //Save it to req.session.zybID in /saveZybriq (save-routes.js).
+//   //and then just use that here.
 
-  res.redirect("restore?savedZib=" + savedZybriq);
-}); //End of /loadState.
+//   res.redirect("restore?savedZib=" + savedZybriq);
+// }); //End of /loadState.
 
 //Listening for port.
 let port = process.env.PORT;
